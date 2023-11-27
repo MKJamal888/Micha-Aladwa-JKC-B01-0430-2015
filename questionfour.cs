149baa4bc942feebd90374fb3780ee5560e30e20
Michael Aladwa <mikealadwa@gmail.com>
2023-11-27 19:17:41 +0300

    question four is solved

---
+++ questionfour.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void PrintString(string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    static void Main()
+    {
+        string myString = "This is Application Programming";
+        PrintString(myString);
+    }
+}
